47de347e5be31fa3a926db078ea3c2650e205bee
yukimi.y <yyy.yukimi@gmail.com>
2021-03-20 16:29:28 +0900

    UserモデルとCommentモデルのアソシエーション設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
   has_many :posts, dependent: :destroy
   has_many :likes
+  has_many :comments
   
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
