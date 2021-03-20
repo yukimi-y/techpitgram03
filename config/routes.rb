fa0f989119b8b22e003544fcf864b0bf0a891274
yukimi.y <yyy.yukimi@gmail.com>
2021-03-20 11:49:08 +0900

    投稿一覧ページを「http://localhost:3000」にアクセスしたら表示できるようにルーティングを設定

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users,
     controllers: { registrations: 'registrations' }
 
-  root 'pages#home'
+  root 'posts#index'
 
   get '/users/:id', to: 'users#show', as: 'user'
 
