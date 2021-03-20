c3253b90df932e1035eadf55027fdd097b73fd36
yukimi.y <yyy.yukimi@gmail.com>
2021-03-20 12:00:34 +0900

    postsコントローラーのshowアクションのルーティングを投稿詳細ページのルーティングとして設定

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,6 @@
 class PostsController < ApplicationController
+  before_action :authenticate_user!
+
   def new
     @post = Post.new
     @post.photos.build
@@ -18,6 +20,10 @@ class PostsController < ApplicationController
 
   def index
     @posts = Post.limit(10).includes(:photos, :user).order('created_at DESC')
+  end
+
+  def show
+    @post = Post.find_by(id: params[:id])
   end
 
   private
--- config/routes.rb
+++ config/routes.rb
@@ -6,8 +6,7 @@ Rails.application.routes.draw do
 
   get '/users/:id', to: 'users#show', as: 'user'
 
-  resources :posts, only: %i(new create index) do
+  resources :posts, only: %i(index new create show) do
     resources :photos, only: %i(create)
   end
-
 end
