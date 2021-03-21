d7c9e196d1c37013e86fb6425b9037900eb518b2
yukimi.y <yyy.yukimi@gmail.com>
2021-03-21 12:38:47 +0900

    ユーザーネーム登録のためのビューとカラム追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_20_072236) do
+ActiveRecord::Schema.define(version: 2021_03_21_032810) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -58,8 +58,9 @@ ActiveRecord::Schema.define(version: 2021_03_20_072236) do
     t.datetime "remember_created_at"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.string "name"
+    t.string "name", default: "", null: false
     t.string "profile_photo"
+    t.string "username", default: "", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
